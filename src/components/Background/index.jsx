8d8d7a630640b51fcb17c92cedc5b5ebed388e9a
JeremyFan <jeremyfan_14@live.com>
2017-10-06 22:47:06 +0800

    great bg

---
+++ src/components/Background/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './index.styl'
+
+const Background = props => (
+  <div className={styles.greatBg}>
+    <img className={styles.bgImage} src="http://p1.music.126.net/hRw8KUtLehbaISu3TAumJQ==/79164837203288.jpg" />
+    <div className={styles.bgGradient}></div>
+  </div>
+)
+export default Background
