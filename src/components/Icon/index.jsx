9ada6b0036d59290715b528cb7a5936f7c0a2421
Jeremy Fan <jeremyfan_14@live.com>
2017-10-17 00:14:36 +0800

    Icon组件接收class

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 import './iconfont'
 import styles from './index.styl'
 
-const Icon = (props) => (
-  <svg className={styles.icon} aria-hidden="true">
-    <use xlinkHref={"#icon-" + props.id}></use>
-  </svg>
-)
+const Icon = (props) => {
+  const className = props.className ? [styles.icon, props.className].join(' ') : styles.icon
+
+  return (
+    < svg className={className} aria-hidden="true">
+      <use xlinkHref={"#icon-" + props.id}></use>
+    </svg >
+  )
+}
 
 export default Icon
