0dc0e90dee7b3703186a156732fdad09bd237d7c
Jeremy Fan <jeremyfan_14@live.com>
2017-10-18 01:24:15 +0800

    当前播放标记

--- src/components/Song/index.jsx
+++ src/components/Song/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
+import Icon from '../Icon'
 import styles from './index.styl'
 import { getDuration } from '../../util'
 
 
 const Song = props => {
   const arNames = props.ar.map(ar => ar.name)
-
   const liClassName = props.isActive ? [styles.song, styles.active].join(' ') : styles.song
 
   return (
     <li className={liClassName} onClick={props.onClick}>
-      <div className={styles.songIndex}>{props.index + 1 + '.'}</div>
+      <div className={styles.songIndex}>
+        <span className={styles.index}>{props.index + 1 + '.'}</span>
+        <Icon id={props.isActive ? "pause" : "play"} className={styles.play} />
+      </div>
       <div className={styles.songInfo}>
         <div className={styles.name}>{props.name}</div>
         <div className={styles.more}>
