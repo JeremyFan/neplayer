281209ca5903b31c89cd07e79137a9a5e3b4e7f4
Jeremy Fan <jeremyfan_14@live.com>
2017-10-17 22:55:28 +0800

    播放条

--- src/components/PlayerBar/index.jsx
+++ src/components/PlayerBar/index.jsx
@@ -5,6 +5,9 @@
 import React, { Component } from 'react'
 
 import styles from './index.styl'
+import Icon from '../Icon'
+
+import { getPicUrl } from '../../util'
 
 class PlayerBar extends Component {
   constructor(props) {
@@ -20,9 +23,14 @@ class PlayerBar extends Component {
     return (
       <div className={styles.bar}>
         <div className={styles.info}>
-          <img className={styles.pic} src={song.al.picUrl} />
+          <img className={styles.pic} src={getPicUrl(song.al.picUrl, 70)} />
           <div className={styles.title}>{song.name}</div>
           <div className={styles.desc}>{ar.join('/')}</div>
+        </div>
+        <div className={styles.controls}>
+          <Icon className={styles.play} id="play" />
+          <Icon className={styles.pause} id="pause" />
+          <Icon id="volume" />
         </div>
       </div>
     )
