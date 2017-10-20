913b4178cab6b5f40eb48cfac3ca323313f00588
Jeremy Fan <jeremyfan_14@live.com>
2017-10-20 23:52:57 +0800

    更新Icon组件

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import './iconfont'
+
 import styles from './index.styl'
 
 const Icon = (props) => {
   const className = props.className ? [styles.icon, props.className].join(' ') : styles.icon
 
   return (
-    < svg className={className} aria-hidden="true">
+    <svg className={className} aria-hidden="true" onClick={props.onClick}>
       <use xlinkHref={"#icon-" + props.id}></use>
     </svg >
   )
