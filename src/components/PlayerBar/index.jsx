ee1daee37b321fb16ca5366a103e44a614af6492
Jeremy Fan <jeremyfan_14@live.com>
2017-10-22 23:25:56 +0800

    播放条

--- src/components/PlayerBar/index.jsx
+++ src/components/PlayerBar/index.jsx
@@ -12,14 +12,23 @@ import { getPicUrl } from '../../util'
 class PlayerBar extends Component {
   constructor(props) {
     super(props)
+
+    window.player
+      .on('player:play', songId => {
+        props.playSong(songId)
+      })
+      .on('player:pause', songId => {
+        props.pause(songId)
+      })
   }
 
   render() {
-    const { song } = this.props
+    const { song, mode, playing } = this.props
 
     if (!song) return null
 
     const ar = song && song.ar.map(a => a.name)
+    const modeName = ['list', 'single', 'random'][mode]
     return (
       <div className={styles.bar}>
         <div className={styles.info}>
@@ -28,14 +37,43 @@ class PlayerBar extends Component {
           <div className={styles.desc}>{ar.join('/')}</div>
         </div>
         <div className={styles.controls}>
-          <Icon className={styles.play} id="play" />
-          <Icon className={styles.pause} id="pause" />
-          <Icon id="volume" />
+          <Icon className={styles.prev} id="prev" onClick={() => this.prev()} />
+          <Icon className={styles.play} id={playing ? "pause" : "play"} onClick={() => this.togglePlay(song.id)} />
+          {/* <Icon className={styles.pause} id="pause" /> */}
+          <Icon className={styles.next} id="next" onClick={() => this.next()} />
+        </div>
+        <div className={styles.more}>
+          <Icon className={styles.mode} id={"mode-" + modeName} onClick={() => this.changeMode()} />
+          <Icon className={styles.volume} id="volume" />
+          <div className={styles.time}>
+            00:50 / 04:02
+          </div>
         </div>
       </div>
     )
   }
 
+  changeMode() {
+    const { mode } = this.props
+    this.props.changeMode(mode)
+  }
+
+  togglePlay(songId) {
+    const { playing } = this.props
+    if (playing) {
+      window.player.pause(songId)
+    } else {
+      window.player.play(songId)
+    }
+  }
+
+  prev() {
+    window.player.prev()
+  }
+
+  next() {
+    window.player.next()
+  }
 }
 
 export default PlayerBar
